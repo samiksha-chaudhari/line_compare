b3673dc72d45656d97e0c85065c04e09cdfbd14c
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 21:14:28 +0530

    distance

--- line_compare/Program.cs
+++ line_compare/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace line_compare
+class DISTANCE
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	// Function to calculate distance
+	static double distance(int x1, int y1, int x2, int y2)
+	{
+		// Calculating distance
+		return Math.Sqrt(Math.Pow(x2 - x1, 2) +
+					Math.Pow(y2 - y1, 2) * 1.0);
+	}
+
+	public static void Main()
+	{
+		Console.WriteLine(Math.Round(distance(10, 8, 12, 6)
+								* 100000.0) / 100000.0);
+	}
 }
